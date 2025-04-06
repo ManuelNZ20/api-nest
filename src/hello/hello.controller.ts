ef556b264618adb95333d5bf97fc33567f2a9551
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2025-04-06 14:39:43 -0500

    http status error

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,13 +1,30 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 // @Controller('hello') // localhost:3000/hello: Para acceder a esta ruta
 @Controller()
 export class HelloController {
-  @Get('/')
+  @Get('/hello')
   index(@Req() request: Request, @Res() response: Response) {
     console.log(request.url); // localhost:3000/hello
     response.status(200).json({
       message: 'Hello World!',
     });
+  }
+  @Get('notfound') // localhost:3000/notFound
+  @HttpCode(404) // Cambia el código de estado de la respuesta a 404 indicando que no se encontró la página
+  notFoundPage() {
+    return '404 not found';
+  }
+
+  @Get('new') // localhost:3000/somethingNew
+  @HttpCode(201) // Cambia el código de estado de la respuesta a 201 indicando que se ha creado un nuevo recurso
+  someThingNew() {
+    return 'Something new';
+  }
+
+  @Get('error')
+  @HttpCode(500) // Cambia el código de estado de la respuesta a 500 indicando que hubo un error interno del servidor
+  errorPage() {
+    return 'Error route';
   }
 }
